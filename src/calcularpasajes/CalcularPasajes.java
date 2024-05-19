1b6a07f7053a9026b835a0090d852bdae620fe3c
Julio Nieto <julionieto213@gmail.com>
2024-05-19 13:44:40 -0500

    segundo commit

--- src/calcularpasajes/CalcularPasajes.java
+++ src/calcularpasajes/CalcularPasajes.java
@@ -15,30 +15,37 @@ public class CalcularPasajes {
         System.out.println("----------------------------COMPANIA DE TURISMO ELEVADOS--------------------------");
         System.out.println("Ingrese los siguientes datos para generar su Factura del Pasaje.");
         
+        //Entrada Nombre
         System.out.println("Ingrese su nombre");
         Nombre = sc2.nextLine();
         System.out.println("\r");
 
+        //Entrada Tarifa
         System.out.println("Ingrese la tarifa Basica");
         Scanner sc = new Scanner(System.in);
         tarifa = sc.nextDouble();
         tarifaBasica = tarifa;
         System.out.println("\r");
 
+        //Entrada Temporada
         System.out.println("Ingrese la temporada. ALTA / BAJA");
         Temporada = sc2.nextLine();
         System.out.println("\r");
 
+        //Entrada Compañia
         System.out.println("Ingrese la compañia para de su preferencia. VOLAR / ALAS");
         Compañia = sc2.nextLine();
 
+        //Si eligio la temporada ALTA
         if (Temporada.equals(Alta)) {
+            //Si eligio la compañia ALAS
             if (Compañia.equals(ALAS)) {
                 double tarifaParcial1 = tarifaBasica * 0.20;
                 double tarifaTotalParcial1 = tarifaBasica - tarifaParcial1;
                 tarifaBasica = tarifaTotalParcial1;
                 System.out.println("Aplica 20% de descuento en la tarifa");
             }
+            //Si eligio la compañia VOLAR
             if (Compañia.equals(Volar)) {
                 double tarifaParcial2 = tarifaBasica * 0.30;
                 double tarifaTotalParcial2 = tarifaBasica - tarifaParcial2;
@@ -49,9 +56,11 @@ public class CalcularPasajes {
         }
         System.out.println("\r");
 
+        //Entrada Edad
         System.out.println("Ingrese su edad");
         Scanner sc3 = new Scanner(System.in);
         edad = sc3.nextInt();
+        //Si es menor de edad
         if (edad < 18) {
             double tarifaParcial3 = tarifaBasica * 0.50;
             double tarifaTotalParcial3 = tarifaBasica - tarifaParcial3;
@@ -60,12 +69,17 @@ public class CalcularPasajes {
         }
         System.out.println("\r");
 
+        //Entrada Estudiante
         System.out.println("¿Es estudiante? Escriba true o false");
         Scanner sc4 = new Scanner(System.in);
         Estudiante = sc4.nextBoolean();
+        //Si es estudiante
         if (Estudiante == true) {
+            //Si es mayor o igual a 18 años
             if (edad >= 18) {
+                //Si eligio la compañia ALAS
                 if (Compañia.equals(ALAS)) {
+                    //Si eligio la temporada BAJA 
                     if (Temporada.equals(Baja)) {
                         double tarifaParcial4 = tarifaBasica * 0.10;
                         double tarifaTotalParcial4 = tarifaBasica - tarifaParcial4;
@@ -77,8 +91,8 @@ public class CalcularPasajes {
         }
         System.out.println("\r");
         tarifaFinal = tarifaBasica;
+        //Formato de salida de las entradas en forma de Factura siguiendo el tema de la actividad Empresa de Turismo.
         System.out.println("----------------------------------------------------------------------------------");
-
         System.out.println("----------------------------------------------");
         System.out.println("--------------------FACTURA-------------------");
         System.out.println("Nombre: " + Nombre);
@@ -89,9 +103,6 @@ public class CalcularPasajes {
         System.out.println("Estudiante: " + Estudiante);
         System.out.println("Tarifa total con descuentos: " + tarifaFinal);
         System.out.println("----------------------------------------------");
-        System.out.println("----------------------------------------------");
-        
-        
+        System.out.println("----------------------------------------------");    
     }
-
 }
